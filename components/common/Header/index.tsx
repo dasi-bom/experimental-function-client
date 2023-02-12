ae6f8ff7891aea26466a719752cd2fd9b89e505b
sunmin <sunm309@naver.com>
2023-02-13 00:56:00 +0900

    fix: header, layout style 수정

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,10 +1,21 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 // style
 import { HeaderWrapper } from './styled';
 
 const Header = () => {
-  return <HeaderWrapper>헤더 테스트</HeaderWrapper>;
+  const router = useRouter();
+  return (
+    <HeaderWrapper>
+      <div className="header-inner">
+        <p className="app-title" onClick={() => router.push('/')}>
+          다시, 봄
+        </p>
+        <div className="search" />
+      </div>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
