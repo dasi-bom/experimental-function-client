d92b6ec6f49082d5bb179b8460ca45ac0113b3f5
Dangbok <ghdwldnjs99@naver.com>
2023-02-12 15:22:21 +0900

    initial commit

--- components/DiaryWrite.tsx
+++ components/DiaryWrite.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const DiaryWrite = ({ diaryId }: IProps) => {
   console.log('diaryId ==> ', diaryId);
-  return <>{diaryId ? <h1>일기 수정 페이지</h1> : <h1>일기 작성 페이지</h1>}</>;
+  return <>{diaryId ? <h1>일기 수정 페이</h1> : <h1>일기 작성 페이지</h1>}</>;
 };
 
 export default DiaryWrite;
