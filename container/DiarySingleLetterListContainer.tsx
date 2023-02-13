28bedb596088b3551f8ba3410a50c51b1cdff8cd
sunmin <sunm309@naver.com>
2023-02-13 22:55:36 +0900

    fix: 메인, 다시보기 페이지 수정

---
+++ container/DiarySingleLetterListContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// component
+import Layout from 'components/common/Layout/Layout';
+import DiarySingleLetterList from 'components/letterList';
+
+const DiarySingleLetterListContainer = () => {
+  return (
+    <Layout>
+      <DiarySingleLetterList />
+    </Layout>
+  );
+};
+
+export default DiarySingleLetterListContainer;
---
+++ container/HomeContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// component
+import Home from 'components/home';
+import Layout from 'components/common/Layout/Layout';
+
+const HomeContainer = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default HomeContainer;
