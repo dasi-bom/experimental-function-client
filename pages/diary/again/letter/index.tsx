80e14d77ee9df29d574e0d35fd12a58b157eaa12
sunmin <sunm309@naver.com>
2023-02-13 22:55:09 +0900

    fix: pages 구조 수정

---
+++ pages/diary/again/letter/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DiarySingleLetterContainer from 'container/DiarySingleLetterContainer';
+
+const index = () => {
+  return (
+    <>
+      <DiarySingleLetterContainer />
+    </>
+  );
+};
+
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import DiaryListContainer from 'container/DiaryListContainer';
+import HomeContainer from 'container/HomeContainer';
 
 // 기본 페이지, 메인
 const Home: NextPage = () => {
   return (
     <>
-      <DiaryListContainer />
+      <HomeContainer />
     </>
   );
 };
