19354cc686ad05d95e46c5b370e065fbc9f5dc1e
Dangbok <ghdwldnjs99@naver.com>
2023-02-15 05:47:46 +0900

    #8 feat: 일기쓰기 페이지 작성

--- components/DiaryWrite.tsx
+++ components/DiaryWrite.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+/* eslint-disable padded-blocks */
+import React, { useRef, useState } from 'react';
+
+// style
+import { DiaryWriteWrapper } from './diary/write/styled';
+
+// icon
+import { AiFillSmile, AiFillMeh, AiFillFrown, AiFillPlusCircle } from 'react-icons/ai';
 
 interface IProps {
   diaryId: any;
@@ -6,7 +13,60 @@ interface IProps {
 
 const DiaryWrite = ({ diaryId }: IProps) => {
   console.log('diaryId ==> ', diaryId);
-  return <>{diaryId ? <h1>일기 수정 페이지</h1> : <h1>일기 작성 페이지</h1>}</>;
+
+  // return <>{diaryId ? <h1>일기 수정 페이지</h1> : <h1>일기 작성 페이지</h1>}</>;
+  
+  return (
+    <DiaryWriteWrapper>
+      <div className="write-wrap">
+        <div className="user-inner">
+          <span>누구의 일기인가요?</span>
+          <div className="user-bg1"></div>
+          <span>곰곰</span>
+        </div>
+        <div className="topic">
+          임보 동물과 함께 할 수 있는 나만의 놀이를 알려주세요~
+        </div>
+        <div className="write-title">
+          <span>제목</span>
+          <input type="text" className="inputfield-title" placeholder="ex) 곰곰이와 0일째"></input>
+        </div>
+        <div className="write-content">
+          <span>본문</span>
+          <input type="text" className="inputfield-content" placeholder=""></input>
+        </div>
+        <div className="write-photo">
+          <span>사진</span>
+          <input 
+            type="file" 
+            className="photo-input" 
+            accept="image/*"
+            multiple={true} 
+            id="profileImg"
+          ></input>
+          <label 
+            className="photo-label" 
+            htmlFor="profileImg"
+          >
+            <AiFillPlusCircle></AiFillPlusCircle></label>
+        </div>
+        <div className="write-stamp">
+          <span>다시 봄 스탬프</span>
+        </div>
+        <div className="write-stamp-inner">
+          <input type="checkbox" className="stamp-btn1" autoComplete="off"></input>
+          <label htmlFor="stamp-btn1"><AiFillSmile></AiFillSmile></label>
+          <input type="checkbox" className="stamp-btn2" autoComplete="off"></input>
+          <label htmlFor="stamp-btn1"><AiFillMeh></AiFillMeh></label>
+          <input type="checkbox" className="stamp-btn3" autoComplete="off"></input>
+          <label htmlFor="stamp-btn1"><AiFillFrown></AiFillFrown></label>
+        </div>
+        <div className="upload-btn" onClick={() => {}}>
+            등록
+        </div>
+      </div>
+    </DiaryWriteWrapper>
+  );
 };
 
 export default DiaryWrite;
--- components/diary/write/styled.tsx
+++ components/diary/write/styled.tsx
@@ -1,3 +1,154 @@
 import styled from 'styled-components';
 
-export const DiaryWriteWrapper = styled.div``;
+export const DiaryWriteWrapper = styled.div`
+.write-wrap {
+    width: 100%;
+    height: 100vh;
+  }
+  .user-inner {
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 12px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+        div{
+            cursor:pointer;
+        }
+   }
+}
+  .user-bg1 {
+    background-image: url('../../images/lucky.jpg');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+  }
+  .topic {
+    background-color: #ffa;
+    width: 100%;
+    height: 45px;
+    margin: 0 auto;
+    border: 3px solid #ffa;
+    border-radius: 20px;
+    padding: 15px 10px 0 10px;
+    text-align:center;
+    font-size:12px;
+  }
+  .write-title {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+    }
+   .inputfield-title {
+    background-color:white;
+    width:100%;
+    height: 45px;
+   }
+ }
+   .write-content {
+    width: 100%;
+    height: 190px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+   .inputfield-content {
+    background-color:white;
+    width:100%;
+    height: 131px;
+   }
+ }
+ .write-photo {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+   .photo-label {
+    margin: 15px 15px 30px 50px;
+    font-weight: bold;
+    font-size: 15px;
+    color: #0095f6;
+    display: inline-block;
+    cursor: pointer;
+  }
+  // input태그
+  .photo-input[type="file"] {
+    display: none;
+  }
+ }
+ .write-stamp {
+    width: 100%;
+    height: 55px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+ }
+ .write-stamp-inner {
+    width: 100%;
+    height: 55px;
+    display: flex;
+    justify-content: space-evenly;
+   .stamp-btn1 {
+   
+   }
+   .stamp-btn2 {
+    
+   }
+   .stamp-btn3 {
+    
+   }
+ }
+ .upload-btn {
+    background-color: #ffa;
+    width: 20%;
+    height: 40px;
+    margin: 0 auto;
+    border: 3px solid #ffa;
+    border-radius: 20px;
+    padding: 15px 10px 0 10px;
+    text-align:center;
+    font-size:12px;
+  }
+`;
