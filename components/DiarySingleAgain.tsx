dcdf7e1b0495ede85907fc490c578bdf972bec05
sunmin <sunm309@naver.com>
2023-02-19 23:51:10 +0900

    #11 feat: 다시보기 페이지 퍼블리싱, 슬라이드 적용

--- components/DiarySingleAgain.tsx
+++ components/DiarySingleAgain.tsx
@@ -1,28 +1,71 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 
+// slick
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 // style
 import { DiarySingleAgainWrapper } from './diary/again/styled';
 
 const DiarySingleAgain = () => {
   const router = useRouter();
 
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+  };
+
   return (
     <DiarySingleAgainWrapper>
-      <div className="again-inner">
-        <div className="again-image"></div>
-        <div className="again-content-wrap">
-          <div className="again-title">곰곰 다이어리</div>
-          <div className="again-content">
-            곰곰이는 00님과
-            <br /> 22년 2월 1일부터 23년 1월 31일까지
-            <br /> 365일 함께 했어요
-          </div>
-          <div className="step-btn" onClick={() => router.push('/diary/again/letter')}>
-            곰곰이와의 추억을 열어보세요 {'>'}
+      <Slider {...settings}>
+        <div className="again-inner">
+          <div className="again-image"></div>
+          <div className="again-content-wrap">
+            <div className="again-title">곰곰 다이어리</div>
+            <div className="again-content">
+              곰곰이는 00님과
+              <br /> 22년 2월 1일부터 23년 1월 31일까지
+              <br /> 365일 함께 했어요
+            </div>
+            <div className="step-btn" onClick={() => router.push('/diary/again/letter')}>
+              곰곰이와의 추억을 열어보세요 {'>'}
+            </div>
           </div>
         </div>
-      </div>
+
+        <div className="again-inner">
+          <div className="again-image again-image2"></div>
+          <div className="again-content-wrap">
+            <div className="again-title">달리 다이어리</div>
+            <div className="again-content">
+              달리는 00님과
+              <br /> 22년 2월 1일부터 23년 1월 31일까지
+              <br /> 365일 함께 했어요
+            </div>
+            <div className="step-btn" onClick={() => router.push('/diary/again/letter')}>
+              달리와의 추억을 열어보세요 {'>'}
+            </div>
+          </div>
+        </div>
+
+        <div className="again-inner">
+          <div className="again-image again-image3"></div>
+          <div className="again-content-wrap">
+            <div className="again-title">밀라 다이어리</div>
+            <div className="again-content">
+              밀라는 00님과
+              <br /> 22년 2월 1일부터 23년 1월 31일까지
+              <br /> 365일 함께 했어요
+            </div>
+            <div className="step-btn" onClick={() => router.push('/diary/again/letter')}>
+              밀라와의 추억을 열어보세요 {'>'}
+            </div>
+          </div>
+        </div>
+      </Slider>
     </DiarySingleAgainWrapper>
   );
 };
--- components/letterList/index.tsx
+++ components/letterList/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+
+// slick
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 // style
 import { DiarySingleLetterListWrapper } from './styled';
 
 const DiarySingleLetterList = () => {
-  return <DiarySingleLetterListWrapper>다시보기3</DiarySingleLetterListWrapper>;
+  const router = useRouter();
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+  };
+
+  const tmpArr = Array.from({ length: 5 }, (_: any, key: number) => {
+    return {
+      date: `23.01.25 17:0${key}`,
+      title: `#곰곰이와 산책했던 시간${key + 1}`,
+      content:
+        '오늘 곰곰이랑 양평 강아지 놀이터에 갔다. 곰곰이가 다른 강아지랑 잘 놀아서 얼마나 좋던지!😊',
+    };
+  });
+
+  return (
+    <DiarySingleLetterListWrapper>
+      <div className="title-wrap">
+        <h3>곰곰이와 산책했던 시간</h3>
+      </div>
+      <Slider {...settings}>
+        {tmpArr && tmpArr.length
+          ? tmpArr.map((item: any, key: number) => (
+              <div key={key} className="slide-wrap carousel">
+                <p className="date">{item.date}</p>
+                <p className="title">{item.title}</p>
+                <div className="content-img" />
+
+                <p className="content">{item.content}</p>
+
+                <img src="/images/share.png" alt="" />
+              </div>
+            ))
+          : ''}
+      </Slider>
+      <div className="ios-share-img">
+        <img src="/images/ios-share.png" alt="" />
+      </div>
+      <div className="bottom-btn" onClick={() => router.push('/diary')}>
+        <span>곰곰이 일기 다시보기</span>
+      </div>
+    </DiarySingleLetterListWrapper>
+  );
 };
 
 export default DiarySingleLetterList;
