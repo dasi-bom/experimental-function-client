c4c450cce9a5f63635013be81ec0ade8bd12031e
Dangbok <101463032+Dangbok@users.noreply.github.com>
2023-02-22 01:43:42 +0900

    #8 feat: 일기쓰기 페이지 작성 (#9)
    * #8 feat: 일기쓰기 페이지 작성
    
    * #8 fix: asset 수정
    
    * #8 fix: state 작업
    
    * #8 fix: state 수정
    
    * #8 fix: state 수정
    
    * #8 fix: stamp 수정
    
    * #8 fix: stamp 수정

--- components/DiaryWrite.tsx
+++ components/DiaryWrite.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+/* eslint-disable padded-blocks */
+import React, { useEffect, useState } from 'react';
+
+// style
+import { DiaryWriteWrapper } from './diary/write/styled';
+
+// axios(서버연결)
+import axios from 'axios';
 
 interface IProps {
   diaryId: any;
@@ -6,7 +13,136 @@ interface IProps {
 
 const DiaryWrite = ({ diaryId }: IProps) => {
   console.log('diaryId ==> ', diaryId);
-  return <>{diaryId ? <h1>일기 수정 페이지</h1> : <h1>일기 작성 페이지</h1>}</>;
+
+  // state 선언
+  const [diaryForm, setDiaryForm] = useState({ title: '', content: '' });
+  const [file, setFile] = useState([]);
+  const [stamptype, setStampType] = useState<any>([]);
+
+  // diaryForm(title과 content)
+  const handlediaryformChange = (e: any) => {
+    setDiaryForm({
+      ...diaryForm, 
+      [e.target.name]: e.target.value
+    });
+    console.log(diaryForm); // title, content 확인 완료(단독시)
+  };
+  
+  // file(image file)
+  const handlefileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    
+    if (e.target.files) {
+      const uploadFile = e.target.files[0];
+      formData.append('file', uploadFile);
+      setFile(uploadFile);
+      console.log(uploadFile); // file 확인 완료(단독시)
+    }
+  };
+
+  // stamptype(stamp) 
+  const handlestamptypeChange = (e: any) => {
+    // console.log(e.currentTarget.dataset); // stamptype 확인 완료(단독시)
+    const { type } = e.currentTarget.dataset;
+    let tmpArr = [...stamptype];
+    tmpArr.push({ stampType: type });
+    setStampType(tmpArr);
+    console.log(tmpArr);
+  };
+ 
+  // 등록 onClick시 서버로 전송
+  const onClickSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('file', file[0]);
+    formData.append('stamptype', stamptype);
+
+    // 서버로 보내는 API
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/DiaryWrite',
+      data: formData,
+    })
+    .then((result) => {
+ console.log('일기 등록에 성공했습니다.');
+    console.log(result);
+  })
+    .catch((error) => {
+ console.log('일기 등록에 실패했습니다.');
+    console.log(error);   
+  });
+  };
+
+  // return <>{diaryId ? <h1>일기 수정 페이지</h1> : <h1>일기 작성 페이지</h1>}</>;
+  
+  return (
+    <DiaryWriteWrapper>
+      <div className="write-wrap">
+        <div className="user-inner">
+          <span>누구의 일기인가요?</span>
+          <div className="user-bg1"></div>
+          <span>곰곰</span>
+        </div>
+        <div className="topic">
+          임보 동물과 함께 할 수 있는 나만의 놀이를 알려주세요~
+        </div>
+        <div className="write-title">
+          <span>제목</span>
+          <input 
+            name="title"  
+            type="text" 
+            className="inputfield-title" 
+            placeholder="ex) 곰곰이와 0일째"
+            onChange={handlediaryformChange}
+          />
+        </div>
+        <div className="write-content">
+          <span>본문</span>
+          <textarea 
+            name="content" 
+            className="inputfield-content" 
+            placeholder="자유롭게 작성하세요:)"
+            onChange={handlediaryformChange}
+          >
+          </textarea>
+        </div>
+        <form>
+        <div className="write-photo">
+          <span>사진</span>
+          <label 
+            className="photo-label" 
+            htmlFor="profileImg"
+          ><img style={{ width: '40px' }} src="/images/add-photo.png" id="image" />
+          </label>
+          <input 
+            type="file" 
+            className="photo-input" 
+            accept="image/*"
+            multiple={true} 
+            id="profileImg"
+            onChange={handlefileChange}
+          ></input>
+        </div>
+        </form>
+        <div className="write-stamp">
+          <span>다시 봄 스탬프</span>
+        </div>
+        <div className="write-stamp-inner">
+        <img src="/images/stamp4.png" data-type={'WALK'} onClick={handlestamptypeChange}/>
+        <img src="/images/stamp1.png" data-type={'TREAT'} onClick={handlestamptypeChange}/>
+        <img src="/images/stamp3.png" data-type={'TOY'} onClick={handlestamptypeChange}/>
+        <img src="/images/stamp2.png" data-type={'TRAVEL'} onClick={handlestamptypeChange}/>
+        </div><br/><br/>
+
+        <div className="upload-btn" onClick={handlestamptypeChange}>
+            등록
+        </div>
+      </div>
+    </DiaryWriteWrapper>
+  );
 };
 
 export default DiaryWrite;
--- components/diary/write/styled.tsx
+++ components/diary/write/styled.tsx
@@ -1,3 +1,147 @@
 import styled from 'styled-components';
 
-export const DiaryWriteWrapper = styled.div``;
+export const DiaryWriteWrapper = styled.div`
+.write-wrap {
+    width: 100%;
+    height: 100vh;
+  }
+  .user-inner {
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 12px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+        div{
+            cursor:pointer;
+        }
+   }
+}
+  .user-bg1 {
+    background-image: url('../../images/lucky.jpg');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+  }
+  .topic {
+    background-color: #ffa;
+    width: 100%;
+    height: 45px;
+    margin: 0 auto;
+    border: 3px solid #ffa;
+    border-radius: 20px;
+    padding: 15px 10px 0 10px;
+    text-align:center;
+    font-size:12px;
+  }
+  .write-title {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+    }
+   .inputfield-title {
+    background-color:white;
+    width:100%;
+    height: 45px;
+    border: 1.5px solid grey;
+   }
+ }
+   .write-content {
+    width: 100%;
+    height: 190px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+   .inputfield-content {
+    background-color:white;
+    width:100%;
+    height: 131px;
+   }
+ }
+ .write-photo {
+    width: 100%;
+    height: 125px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+   .photo-label {
+    margin: 5px 15px 30px 40px;
+    font-weight: bold;
+    font-size: 15px;
+    color: #0095f6;
+    display: inline-block;
+    cursor: pointer;
+  }
+  // input태그
+  .photo-input[type="file"] {
+    display: none;
+  }
+ }
+ .write-stamp {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    span {
+        font-size: 15px;
+        padding: 20px;
+        font-family: 'JejuGothic';
+        font-style: normal;
+        font-weight: 600;
+   }
+ }
+ .write-stamp-inner {
+    width: 100%;
+    height: 35px;
+    display: flex;
+    justify-content: space-evenly;
+ }
+ .upload-btn {
+    background-color: #ffa;
+    width: 20%;
+    height: 40px;
+    margin: 0 auto;
+    border: 3px solid #ffa;
+    border-radius: 20px;
+    padding: 15px 10px 0 10px;
+    text-align:center;
+    font-size:12px;
+    font-weight: 600;
+  }
+`;
