c090926973c16dfae008aa10c52f444d2af4e529
sunmin <sunm309@naver.com>
2023-03-07 01:18:48 +0900

    #27 feat: 일기 리스트 api 연동, 스크롤 이벤트 추가

--- container/DiaryListContainer.tsx
+++ container/DiaryListContainer.tsx
@@ -5,29 +5,99 @@ import Layout from 'components/common/Layout/Layout';
 import DiaryList from 'components/DiaryList';
 
 // services
-import { getDiaryList } from '../services/diary';
-import { useRouter } from 'next/router';
+import { getDiaryList, getTmpId } from '../services/diary';
 
 const DiaryListContainer = () => {
-  const router = useRouter();
-
   const [diaryList, setDiaryList] = useState<any>([]);
-  const [cursor, setCursor] = useState<number>(0);
-  const [petId, setPetId] = useState<number>(0);
+  const [cursor, setCursor] = useState<any>();
+
+  // 스크롤 이벤트
+  const [pageY, setPageY] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState<number>(0);
+  const [isUpdate, setIsUpdate] = useState<boolean>(false);
+
+  const checkScrollTop = () => {
+    const fullHeight = document.documentElement.scrollHeight;
+    const top = document.documentElement.scrollTop;
+    const height = document.documentElement.clientHeight;
+
+    setPageY(top + height);
+    setScrollHeight(fullHeight);
+
+    if (top + height >= fullHeight) {
+      setIsUpdate(true);
+    } else {
+      setIsUpdate(false);
+    }
+  };
 
   useEffect(() => {
-    // getDiaryList({ cursor, petId })
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
-  }, [cursor, petId]);
+    window.addEventListener('scroll', checkScrollTop);
+    return () => window.removeEventListener('scroll', checkScrollTop);
+  });
+
+  // tmpId 발급
+  useEffect(() => {
+    getTmpId({})
+      .then((res) => {
+        if (res.status === 200) {
+          const data = res.data.message;
+          setCursor(Number(data));
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // 일기 리스트 호출
+  const getDiary = () => {
+    getDiaryList({ cursor })
+      .then((res) => {
+        if (res.status === 200) {
+          const data = res.data.content;
+          if (diaryList.length === 0) {
+            setDiaryList(data);
+          }
+        } else {
+          console.log('getDiaryList fail');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // 맨 아래까지 스크롤 할 경우 다음 일기 리스트 호출
+  useEffect(() => {
+    getDiary();
+
+    if (isUpdate && pageY >= scrollHeight) {
+      diaryList.map((item: any, key: number) => {
+        if (key === diaryList.length - 1) {
+          console.log('ok');
+          setCursor(item.diaryId);
+
+          getDiaryList({ cursor })
+            .then((res) => {
+              if (res.status === 200) {
+                const data = res.data.content;
+                setDiaryList([...diaryList, ...data]);
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      });
+    }
+  }, [isUpdate, cursor]);
+
+  // console.log(diaryList);
 
   return (
     <Layout>
-      <DiaryList diaryList={diaryList} cursor={cursor} petId={petId} />
+      <DiaryList diaryList={diaryList} />
     </Layout>
   );
 };
