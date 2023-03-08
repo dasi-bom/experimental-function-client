58af182f1c54f36751f3ee47af57485c92bbbc41
sunmin <sunm309@naver.com>
2023-03-08 23:30:13 +0900

    #30 feat: 임시보호 종료 버튼 추가, 임시보호 api 연동

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -4,20 +4,39 @@ import React from 'react';
 // style
 import { HomeWrapper } from './styled';
 
-const Home = () => {
+interface IProps {
+  endProtection: () => void;
+  isEnded: boolean;
+  warningMessage: () => void;
+  pet: any;
+}
+
+const Home = ({ endProtection, isEnded, warningMessage, pet }: IProps) => {
   const router = useRouter();
 
   return (
     <HomeWrapper>
       <div className="profile-wrap">
-        <div className="profile-inner">
-          <div className="write-btn" onClick={() => router.push('/diary/write')}>
-            <div className="bg1"></div>
-            <span>일기쓰기</span>
+        <div className="profile-btn-inner">
+          <div className="profile-inner">
+            <div className="write-btn" onClick={() => router.push('/diary/write')}>
+              <div className="bg1"></div>
+              <span>일기쓰기</span>
+            </div>
+            <div
+              className="again-btn"
+              onClick={() =>
+                isEnded
+                  ? router.push(`/diary/again?stampType=WALK&petName=${pet?.petName}`)
+                  : warningMessage()
+              }
+            >
+              <div className="bg2"></div>
+              <span>다시보기</span>
+            </div>
           </div>
-          <div className="again-btn" onClick={() => router.push('/diary/again')}>
-            <div className="bg2"></div>
-            <span>다시보기</span>
+          <div className="finish-btn" onClick={endProtection}>
+            <span>임시보호 종료하기</span>
           </div>
         </div>
       </div>
