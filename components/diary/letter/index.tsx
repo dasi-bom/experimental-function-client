f2e030f5bd55f98f685b783f40f578d17b5f19b8
sunmin <sunm309@naver.com>
2023-03-08 23:33:53 +0900

    #30 fix: router stampType 추가, petName 하드코딩

--- components/diary/letter/index.tsx
+++ components/diary/letter/index.tsx
@@ -6,13 +6,44 @@ import { DiarySingleLetterWrapper } from './styled';
 
 const DiarySingleLetter = () => {
   const router = useRouter();
+  const { stampType } = router.query;
 
   return (
     <DiarySingleLetterWrapper>
       <div className="letter-wrap">
         <div className="letter-title">곰곰이와 산책했던 시간</div>
         <div className="letter-img"></div>
-        <div className="letter-date" onClick={() => router.push('/diary/again/letter/0')}>
+        <div
+          className="letter-date"
+          onClick={() => router.push(`/diary/again/letter/0?stampType=${stampType}&petName=gomgom`)}
+        >
+          23년 1월 25일에 쓴<br />
+          일기가 날아왔어요
+        </div>
+      </div>
+      {}
+      <div className="letter-wrap">
+        <div className="letter-title">당당이와 산책했던 시간</div>
+        <div className="letter-img"></div>
+        <div
+          className="letter-date"
+          onClick={() =>
+            router.push(`/diary/again/letter/0?stampType=${stampType}&petName=dangdang`)
+          }
+        >
+          23년 1월 25일에 쓴<br />
+          일기가 날아왔어요
+        </div>
+      </div>
+      <div className="letter-wrap">
+        <div className="letter-title">봉봉이와 산책했던 시간</div>
+        <div className="letter-img"></div>
+        <div
+          className="letter-date"
+          onClick={() =>
+            router.push(`/diary/again/letter/0?stampType=${stampType}&petName=bongbong`)
+          }
+        >
           23년 1월 25일에 쓴<br />
           일기가 날아왔어요
         </div>
